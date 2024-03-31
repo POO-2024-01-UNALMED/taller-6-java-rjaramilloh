026e82ad89aea43850eb7cfb7662ccb2f3b4a233
rjaramilloh <159493247+rjaramilloh@users.noreply.github.com>
2024-03-31 11:50:49 -0500

    momento 2

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,11 +1,5 @@
 package vehiculos;
 
-import zooAnimales.Anfibio;
-import zooAnimales.Ave;
-import zooAnimales.Mamifero;
-import zooAnimales.Pez;
-import zooAnimales.Reptil;
-
 public class Vehiculo {
 	private String placa;
 	protected int puertas;
